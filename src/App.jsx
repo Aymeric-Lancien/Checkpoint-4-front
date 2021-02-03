919771a007a5def69631b639fe04d129ce4eb83a
Aymeric-Lancien <aymericlancien12@gmail.com>
2021-02-03 12:29:54 +0100

    folder structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world h1</h1>
+      <h2>hello world h2</h2>
+    </div>
+  );
+}
+
+export default App;
